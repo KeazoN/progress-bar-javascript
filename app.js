6c63db18a33e5712cf5b6180affe45a18056c9a8
Uğur <71211251+KeazoN@users.noreply.github.com>
2021-02-04 15:35:52 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+var lookid = document.getElementById("allbar");
+
+var kralkeazon = 0;
+
+
+function keazongorun(){
+	lookid.style.display= "block";
+	if(kralkeazon == 0){
+		kralkeazon = 1;
+		var lookidjunior = document.getElementById("normalbar");
+		var width = 0;
+	    var id = setInterval(frame, 280);
+	    function frame() {
+	      if (width >= 100) {
+	        clearInterval(id);
+	        kralkeazon = 0;
+	      } else {
+	        width++;
+	        lookidjunior.style.width = width + "%";
+	        lookidjunior.innerHTML = width  + "%";
+	      }
+	    }
+	  }
+	}
